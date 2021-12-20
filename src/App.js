83079529e3bd66b93f5286df5c6c027584b2cc24
michele_baruffetti <michele.baruffetti@interact.it>
2021-12-20 16:48:21 +0100

    [feat]added nav and library appear/disappear

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import "./styles/app.scss";
 //import components
 import Song from './components/Song';
 import Player from './components/Player';
+import Nav from "./components/Nav";
 // import songs
 import data from './data.js'
 import Library from "./components/Library";
 
 function App() {
+  // state
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying]= useState(false);
+  const [libraryStatus, setLibraryStatus] = useState(false);
 
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
       <Song currentSong={currentSong}/>
       <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
-      <Library songs={songs} setCurrentSong={setCurrentSong} setIsPlaying={setIsPlaying} setSongs={setSongs}/>
+      <Library  libraryStatus={libraryStatus} songs={songs} setCurrentSong={setCurrentSong} setIsPlaying={setIsPlaying} setSongs={setSongs}/>
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -46,7 +46,7 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input onChange={drugHandler} min={0} max={songInfo.duration} value={songInfo.currentTime} type="range" />
+                <input onChange={drugHandler} min={0} max={songInfo.duration || 0} value={songInfo.currentTime} type="range" />
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
